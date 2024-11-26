d33a33dee21e0f4587a887558a7b608ba37252a8
zinozhang <zinozhang@tencent.com>
2024-11-26 15:03:26 +0800

    feat: support pb3 optional

--- cmd/protoc-gen-go-tableau-checker/main.go
+++ cmd/protoc-gen-go-tableau-checker/main.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 const version = "0.3.0"
@@ -29,6 +30,7 @@ func main() {
 	protogen.Options{
 		ParamFunc: flags.Set,
 	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
