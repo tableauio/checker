c615737d50a66e481c3e718e833b8fa0839135cd
Kybxd <627940450@qq.com>
2026-05-25 14:38:53 +0800

    refactor: use Error() method in ErrorFormatText

--- test/check/error.check.go
+++ test/check/error.check.go
@@ -76,10 +76,7 @@ type ErrorFormat func(*CheckError) string
 var ErrorFormatText ErrorFormat = func(e *CheckError) string {
 	msgs := make([]string, len(e.Issues))
 	for i, issue := range e.Issues {
-		msgs[i] = fmt.Sprintf("error: workbook %s, worksheet %s, custom check failed: %+v",
-			issue.Workbook.GetName(),
-			issue.Worksheet.GetName(),
-			issue.Message)
+		msgs[i] = issue.Error()
 	}
 	return strings.Join(msgs, "\n")
 }
